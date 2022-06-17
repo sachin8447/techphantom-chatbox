b0508c5f90c28f903070b13fa8b48cd671b7645f
Sachin Kumar <sachin8447@gmail.com>
2022-06-18 00:12:39 +0530

    Controllers and Routes Added

--- starter/app.js
+++ starter/app.js
@@ -1,14 +1,27 @@
 const expressServer = require("express");
 const app = expressServer();
 const port = 3000;
+const  tasks =  require('./router/routed');
+const connectDb = require('./db/db');
+require('dotenv').config()
+
+
 
 app.get("/",(req,res)=>{
     res.send(req.params)
 })
 
 
-app.use('app/v1/service')
+app.use('/app/v1/',tasks);
+
+const start = async ()=>{
+try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on : ${port}`));
+} catch (error) {
+    
+}
+}
 
 
-
-app.listen(port, console.log(`Server is listening on : ${port}`));
+start()
